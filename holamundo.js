668ccfc4cb1787bef1f94180f2f7ccebdee915bf
marc9lo <65792416+marc9lo@users.noreply.github.com>
2020-11-03 07:10:28 -0300

    Add files via upload

---
+++ holamundo.js
@@ -0,0 +1,15 @@
+/*
+var nombre="Bart";
+var altura=180;
+
+var concateno= "Se llama: "+ nombre+" y mide: "+ altura;
+
+document.write(concateno); 
+*/
+
+var nombre="Bart";
+var altura=180;
+
+var datosContenedor= document.getElementById("datos");
+
+document.write(datosContenedor); 
